8eecc476d72a73a6b4aee0657c0d7afb0d605d6e
techtorial <info@techtorialacademy.com>
2021-03-16 18:26:08 -0500

    Updated maximize

--- src/test/java/AudiTest.java
+++ src/test/java/AudiTest.java
@@ -21,7 +21,6 @@ public class AudiTest {
         driver= new ChromeDriver();
         softAssert=new SoftAssert();
         driver.manage().window().maximize();
-        driver.manage().window().maximize();
     }
 
 
